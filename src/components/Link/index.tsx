027b7ac1f111b218a4f60fce35a440e1a5b5a003
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-01 21:50:24 -0300

    refact: component Link for receiver a target

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -3,11 +3,12 @@ import NextLink from 'next/link'
 type Props = {
     children: string | JSX.Element | JSX.Element[],
     href: string | any
+    target?: string | any
   }
 
-export default function Link({children , href}: Props) {
+export default function Link({children , href, target}: Props) {
     return (
-        <NextLink href={href} passHref>
+        <NextLink href={href} passHref target={target}>
             {children}
         </NextLink>
     )
