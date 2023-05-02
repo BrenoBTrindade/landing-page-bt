d2798870decfc0ea3dc74d233450feec3bbc5b12
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-01 21:38:00 -0300

    feat: Header component

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {
+    children: string | JSX.Element | JSX.Element[]
+  }
+
+ export default function Title({children}: Props) {
+    return (
+        <h1 className='text-center text-3xl ml-5'>
+            {children}
+        </h1>
+    )
+}
+
