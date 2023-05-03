58628f03b939e732e3fb48982006a42ed508ef0d
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-03 20:15:37 -0300

    refact: image component

--- src/components/ImageComponent/index.tsx
+++ src/components/ImageComponent/index.tsx
@@ -7,7 +7,7 @@ type Props = {
  
  export default function ImageComponent({src, alt}:Props) {
     return (
-       <Image src={src} alt={alt} className="w-2/4 h-[550px] rounded-xl ml-9 mt-8 mb-8" width={500} height={500} />
+       <Image src={src} alt={alt} className="w-2/4 h-[550px] rounded-xl ml-9 mt-8 mb-8 max-md:w-72 max-md:h-72" width={500} height={500} />
     )
 }
 
