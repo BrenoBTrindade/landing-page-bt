52361511f650588fbe514d6ab4c224f0d7dd8424
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-09 20:06:34 -0300

    fix: select tiitle in icon

--- src/components/TogleTheme/index.tsx
+++ src/components/TogleTheme/index.tsx
@@ -12,7 +12,7 @@ export default function TogleTheme () {
     })
 
  return (
-    <div className="hidden sm:block mr-5">
+    <div className="hidden sm:block mr-5 select-none">
         <MoonIcon className="h-7 w-8 -mr-6 animate-bounce text-gray-950 block dark:hidden cursor-pointer  dark:text-gray-100" onClick={togle}/>
         <SunIcon className="h-7 w-8 -mr-6 animate-bounce text-gray-950 hidden dark:block cursor-pointer  dark:text-gray-100" onClick={togle}/>
     </div>
