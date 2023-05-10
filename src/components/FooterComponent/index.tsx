e8ff09e21056f071054d29bfd6852ae3af5abb70
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-10 11:23:59 -0300

    fix: style

--- src/components/FooterComponent/index.tsx
+++ src/components/FooterComponent/index.tsx
@@ -9,8 +9,8 @@ import Paragraph from '../Paragraph'
             <a className=' flex text-sm max-md:ml-8 border-2 duration-500 border-transparent font-bold text-gray-950 text-opacity-60 dark:text-opacity-60 dark:hover:text-opacity-100 hover:text-opacity-100 hover:border-b-black dark:text-gray-100 dark:hover:border-b-white max-md:opacity-100 max-md:text-sm max-md:border-none' href='#topo'> 
                 Voltar ao topo <ArrowUpIcon className="h-5 ml-2 dark:text-gray-100" />
             </a>
-            <div className='max-xl:-mt-7 flex max-md:mt-0.5 max-md:-ml-2'>
-                <a className='dark:hidden' href='https://github.com/BrenoBTrindade' target="-blank">
+            <div className='max-xl:-mt-7 -mt-5 flex justify-center  max-md:mt-0.5 max-md:-ml-2'>
+                <a className='dark:hidden ' href='https://github.com/BrenoBTrindade' target="-blank">
                     <Icons src='/github.svg' size={30} alt=''/>
                 </a>
                 <a className='hidden dark:block' href='https://github.com/BrenoBTrindade' target="-blank">
